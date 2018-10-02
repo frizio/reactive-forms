973c642aa1925c40f8bc1e303d25b9df93f68a0d
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-10-02 12:11:58 +0200

    Create istance than represent the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  
+  registrationForm = new FormGroup({
+    username: new FormControl('frizio'),
+    password: new FormControl(''),
+    conformPassword: new FormControl(''),
+  });
+
 }
